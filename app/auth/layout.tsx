d8d57d9e60c0d297c4a8520311a0f02d2623cc13
nickKengne <nickdk294@gmail.com>
2024-05-13 18:50:12 +0100

    fix error build vercel

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,13 +2,11 @@ import React, { Suspense } from "react";
 
 export default function layout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}:any) {
   return <Suspense fallback={<Loading />}>{children}</Suspense>;
 }
 
-export const Loading = () => {
+ const Loading = () => {
   return (
     <div>
       <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
